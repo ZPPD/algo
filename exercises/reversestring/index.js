03af0010365da3138e9f739e7f15f5951bd4dcf3
ZPPD <zhana.p.d@gmail.com>
2019-07-01 10:17:44 -0400

    reverse a string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // === Solution 1 ===
+  // return str
+  //   .split("")
+  //   .reverse()
+  //   .join("");
+
+  // === Solution 2 ===
+  // let reversed = "";
+  // for (let character of str) {
+  //   reversed = character + reversed;
+  // }
+  // return reversed;
+
+  // === Solution 3 ===
+  return str
+    .split("")
+    .reduce((reversed, character) => character + reversed, "");
+}
 
 module.exports = reverse;
