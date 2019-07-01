ff4d3351981761018c4b17104fd7683c5100f7b2
ZPPD <zhana.p.d@gmail.com>
2019-07-01 10:45:10 -0400

    reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,26 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  // === Solution 1 ===
+  // const reversed = n
+  //   .toString()
+  //   .split("")
+  //   .reverse()
+  //   .join("");
+  // if (n < 0) {
+  //   return parseInt(reversed) * -1;
+  // }
+  // return parseInt(reversed);
+
+  // === Solution 2 ===
+  const reversed = n
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+
+  return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
