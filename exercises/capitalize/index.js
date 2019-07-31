28dbdb8789c6bf540f2d8fc16cd9e0f7f616b19e
ZPPD <zhana.p.d@gmail.com>
2019-07-31 11:36:00 -0400

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,38 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  // === Solution 1 ===
+
+  // let words = [];
+  // const arr = str.split(" ");
+  // arr.forEach(word => {
+  //   const upper = word[0].toUpperCase() + word.slice(1);
+  //   words.push(upper);
+  // });
+  // return words.join(" ");
+
+  // === Solution 2 ====
+
+  // const words = [];
+  // for (let word of str.split(" ")) {
+  //   words.push(word[0].toUpperCase() + word.slice(1));
+  // }
+  // return words.join(" ");
+
+  // ==== Solution 3 ===
+
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
 
 module.exports = capitalize;
